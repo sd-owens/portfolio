0246c29559b86857664de467090f127083c57ab8
Steve Owens <sd.owens@outlook.com>
2020-02-27 13:47:57 -0800

    modified ENV variables and added requireHTTPS module

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
+require('dotenv').config();
+
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (
+    !req.secure &&
+    req.get('x-forwarded-proto') !== 'https' &&
+    process.env.NODE_ENV !== 'development'
+  ) {
+    res.redirect(`https://${req.get('host')}${req.url}`);
+  }
+  next();
+}
+
 const bodyParser = require('body-parser'),
       express    = require('express'),
       path       = require('path'),
@@ -9,31 +23,21 @@ const indexRoutes = require('./routes/index');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.set('port', 3000);
+// app.set('port', 3000);
 
+app.use(requireHTTPS);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(express.static(`${__dirname}/public`));
 
-app.enable('verbose errors');
 
-// function requireHTTPS(req, res, next) {
-//     // The 'x-forwarded-proto' check is for Heroku
-//     if (
-//       !req.secure &&
-//       req.get('x-forwarded-proto') !== 'https' &&
-//       process.env.NODE_ENV !== 'development'
-//     ) {
-//       res.redirect(`https://${req.get('host')}${req.url}`);
-//     }
-//     next();
-//   }
+app.enable('verbose errors');
 
 app.use('/', indexRoutes);
 
-app.listen(process.env.PORT || app.get('port'), process.env.IP, async () => {
+app.listen(process.env.PORT || 3000, process.env.IP, async () => {
   
-  console.log('Express started on http://localhost:' + app.get('port') +
+  console.log('Express started on http://localhost:' + process.env.PORT +
               '; press Ctrl-C to terminate.');
 
 });
